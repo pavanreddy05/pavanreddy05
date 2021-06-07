5d26e1aa43c188e6067429552e6703140d6d89e8
pavanreddy05 <83871475+pavanreddy05@users.noreply.github.com>
2021-06-07 22:16:35 +0530

    Update pyrand.py
    modified print

--- pyrand.py
+++ pyrand.py
@@ -7,8 +7,8 @@ if d.hour >= 12 and d.hour < 4:
     print('Good Afternoon Sir/Madam')
 if d.hour >=16 and d.hour < 20:
     print('Good Evening Sir/Madam')
-if d.hour >= 20 and d.hour < 6:
-    print('Good Sir Sir/Madam')
+if d.hour >= 20 :
+    print('Good Night Sir/Madam')
 print('Take a deep breathe and note down the below number ')
 number = random.randint(1111,9999)
 print('The secret number is : ',number)
