36649d58911fc22ec3bfcb5a8a99efc65a7ea750
pavanreddy05 <83871475+pavanreddy05@users.noreply.github.com>
2021-06-08 09:14:07 +0530

    Update pyrand.py

--- pyrand.py
+++ pyrand.py
@@ -1,14 +1,19 @@
+from flask import Flask
 import random
 import datetime
-d = datetime.datetime.now()
-if d.hour >= 6 and d.hour < 12:
-    print('Good Morning Sir/Madam')
-if d.hour >= 12 and d.hour < 4:
-    print('Good Afternoon Sir/Madam')
-if d.hour >=16 and d.hour < 20:
-    print('Good Evening Sir/Madam')
-if d.hour >= 20 :
-    print('Good Night Sir/Madam')
-print('Take a deep breathe and note down the below number ')
-number = random.randint(1111,9999)
-print('The secret number is : ',number)
+randapp = Flask(__name__)
+@randapp.route("/")
+def rand():
+    d = datetime.datetime.now()
+    if d.hour >= 6 and d.hour < 12:
+        print('Good Morning Sir/Madam')
+    if d.hour >= 12 and d.hour < 4:
+        print('Good Afternoon Sir/Madam')
+    if d.hour >=16 and d.hour < 20:
+        print('Good Evening Sir/Madam')
+    if d.hour >= 20 :
+        print('Good Night Sir/Madam')
+    print('Take a deep breathe and note down the below number ')
+    number = random.randint(1111,9999)
+    print('The secret number is : ',number)
+rand()
